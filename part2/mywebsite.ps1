4b3d9c0f3003187fe118176c5e2bfff48ab8913f
andreireznikau <andrei.reznikau@outlook.com>
2018-03-05 15:35:14 +0300

    Update mywebsite.ps1

--- part2/mywebsite.ps1
+++ part2/mywebsite.ps1
@@ -1,21 +1,33 @@
-Configuration MyWebsite
 {
-  param ($MachineName)
-
-  Node $MachineName
-  {
-    #Install the IIS Role
-    WindowsFeature IIS
+        Ensure = "Present"
+        Name = "Web-Asp-Net45"
+    }
+    
+    #Install IISManagementConsole
+    WindowsFeature WebServerManagementConsole
     {
-      Ensure = "Present"
-      Name = "Web-Server"
+        Name = "Web-Mgmt-Console"
+        Ensure = "Present"
     }
 
-    #Install ASP.NET 4.5
-    WindowsFeature ASP
+    #Replace IIS start page by hostname
+    Script HTMLEdit
     {
-      Ensure = "Present"
-      Name = "Web-Asp-Net45"
+        SetScript = 
+        { 
+            $hostname = get-childitem -path env:computername
+            "<html><body>"+$hostname.value+"</body></html>" | out-file -FilePath "C:\inetpub\wwwroot\iisstart.htm"
+            Start-Process -FilePath C:\Windows\System32\iisreset.exe -ArgumentList /RESTART
+        }
+        TestScript = 
+        {
+            return $False
+        }
+        GetScript = 
+        { 
+            Test-Path -Path "C:\inetpub\wwwroot\iisstart.htm"
+        }
+        DependsOn = "[WindowsFeature]IIS"       
     }
   }
 }
